b8de76773ed13dba71c5b8f9b0715bdf011e3a21
JochenHansoul <jochen.hansoul@outlook.be>
2023-04-03 22:18:17 +0200

    Moved the code to get the subscription in it's own function. added a function to remove all the subscriptons, removed the direct subscription and added eventlisteners to the buttons to subscribe and unsubscribe.

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,65 @@
+/*******************/
+/* constants *******/
+/*******************/
+
 const publicVapidKey = "BEnOCKUAbcQFBfs3YmWr7HtJBPCGlkWYMjlSZVxI83UliaB8PqEuF6UhE6jUDs0qKBcBHLs1-QrLePhLMY-chus";
+
+const subscribeButton = document.getElementById("subscribe");
+const unsubscribeButton = document.getElementById("unsubscribe");
+const outputElement = document.querySelector("output[name=result]");
+
+/*******************/
+/* functions *******/
+/*******************/
+
+const getSubscription = async function () {
+    // register service worker
+    const register = await navigator.serviceWorker
+        .register("./service-worker.js", { scope: "/" });
+
+    // register push
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicVapidKey
+    });
+    return subscription;
+};
+
+const removeSubscriptions = function () {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        registrations.forEach(registration => registration.unregister());
+    });
+    outputElement.innerText = "You have successfully unsubscribed. When you resubscribe the subscription needs to be changed in the back-end as it's hard coded.";
+};
 
 const sendSubscription = async function () {
     // checking for service worker
     if ("serviceWorker" in navigator) {
-        // register service worker
-        const register = await navigator.serviceWorker
-            .register("./service-worker.js", { scope: "/" });
-
-        // register push
-        const subscription = await register.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: publicVapidKey
-        });
+        const subscription = await getSubscription();
 
         // send push
-        await fetch("/subscribe", {
+        const result = await fetch("/subscribe", {
             method: "POST",
             body: JSON.stringify(subscription),
             headers: {
                 "Content-Type": "application/json"
             }
         });
-    } else {
-        console.log("Push Manager is not supporter");
-    }
-}
 
-sendSubscription().catch(error => console.error(error));
+        // feedback for user
+        outputElement.innerText = (result.status === 201)
+            ? "You have successfully subscribed. You should now see a notification."
+            : "An error occured. Status: " + result.status;
+    } else {
+        outputElement.innerText = "Push Manager is not supporter";
+    }
+};
+
+/*******************/
+/* main ************/
+/*******************/
+
+
+subscribeButton.addEventListener("click", () => sendSubscription());
+
+unsubscribeButton.addEventListener("click", () => removeSubscriptions());
