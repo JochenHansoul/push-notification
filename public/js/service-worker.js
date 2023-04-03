5227422dc100cc0e6a76ae7d78fb7d98279f0c28
JochenHansoul <jochen.hansoul@outlook.be>
2023-04-03 18:29:41 +0200

    updated the service-worker so that also the body and icon can be changed

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("push", function (e) {
     const data = e.data.json();
     e.waitUntil(self.registration.showNotification(data.title, {
-        body: "Body of the notification 2",
-        icon: "./assets/images/icons8-globe-24.png"
+        body: data.options.body,
+        icon: data.options.icon
     }));
 });
